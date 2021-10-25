1ab8e071bf80348cc30e24687f387f436fbb0138
ferhat-dogan <ferhat.dogan@ceng.deu.edu.tr>
2021-10-25 19:10:07 +0300

    Initial commit

---
+++ src/main/java/com/demo/appcenttodolist/AppcentTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.demo.appcenttodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcentTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppcentTodolistApplication.class, args);
+    }
+
+}
